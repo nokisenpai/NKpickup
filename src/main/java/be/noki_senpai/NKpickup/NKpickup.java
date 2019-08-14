1b66ae90839a9cf77e2135b6e181aab53d919a13
nokisenpai <nicocorei@hotmail.com>
2019-08-14 22:18:28 +0200

    Initial commit

---
+++ src/main/java/be/noki_senpai/NKpickup/NKpickup.java
@@ -0,0 +1,13 @@
+package be.noki_senpai.NKpickup;
+
+/**
+ * Hello world!
+ *
+ */
+public class NKpickup 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
